5aa33756223a8dc496204600af6351bf7f0dd7ea
Ben2402 <gumasing.benedict@collegeofmaryimmaculate.edu.ph>
2025-11-15 08:55:50 +0800

    Add files via upload

---
+++ activity-week2-dowhile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int j = 0;
+        do
+        {
+            if (j % 2 == 0)
+            {
+                Console.WriteLine(j);
+            }
+        }
+
+        while (j < 20);
+    }
+}
---
+++ activity-week2-while.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int j = 0;
+        while (j < 20)
+        {
+            if (j % 2 == 0)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
