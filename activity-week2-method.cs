8c8d895f9c6c7c8e42b08961341cde8c14bd26c3
Ben2402 <gumasing.benedict@collegeofmaryimmaculate.edu.ph>
2025-11-15 09:52:46 +0800

    Add files via upload

---
+++ activity-week2-method.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static string typeString;
+
+    public static void Main()
+    {
+        Console.WriteLine("Please enter the word passed: ");
+        typeString = Console.ReadLine();
+        stringCheck(typeString);
+    }
+
+    public static void stringCheck(string encryption)
+    {
+        if (typeString.ToLower() == "passed")
+        {
+            Console.WriteLine("encyrpted");
+        }
+
+        else
+        {
+            Console.WriteLine("unenecrypted");
+        }
+    }
+}
